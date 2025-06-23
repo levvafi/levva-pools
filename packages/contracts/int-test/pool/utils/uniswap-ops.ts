b4ba483f1e07d31f343a0d4eaad44cd5414114fa
Pavel <pavsmel@hotmail.com>
2025-06-23 18:47:41 +0300

    fix deps errors

--- packages/contracts/int-test/pool/utils/uniswap-ops.ts
+++ packages/contracts/int-test/pool/utils/uniswap-ops.ts
@@ -1,12 +1,11 @@
 import { formatUnits, parseUnits } from 'ethers';
-import { tickToPrice } from '@uniswap/v3-sdk';
-import { Token } from '@uniswap/sdk-core';
 import { uniswapV3Swapdata } from './chain-ops';
 import { IUniswapV3Pool, IUSDC, IWETH9 } from '../../../../contracts/typechain-types';
 import { MarginlyRouter } from '../../../../router/typechain-types';
 import { abs } from './fixed-point';
 import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers';
 import { Logger } from 'pino';
+import { IERC20Metadata } from '../../../../periphery/typechain-types';
 
 export async function changeWethPrice(
   treasury: SignerWithAddress,
@@ -26,10 +25,8 @@ export async function changeWethPrice(
 ) {
   logger.info(`Start changing price, target: ${targetPrice.toString()}`);
   const { tick } = await uniswap.connect(treasury.provider).slot0();
-  const USDC = new Token(1, usdc.target.toString(), 6, 'USDC');
-  const WETH = new Token(1, weth.target.toString(), 18, 'WETH');
 
-  const wethPrice = BigInt(tickToPrice(WETH, USDC, Number(tick)).toFixed(0));
+  const wethPrice = BigInt(await estimateTickToPrice(weth, usdc, Number(tick)));
   logger.info(`WETH price is ${wethPrice}`);
   logger.info(`WETH balance uniswap ${formatUnits(await weth.balanceOf(uniswap), 18)}`);
   logger.info(`USDC balance uniswap ${formatUnits(await usdc.balanceOf(uniswap), 6)}`);
@@ -49,7 +46,6 @@ export async function changeWethPrice(
     await (await usdc.connect(treasury).approve(swapRouter, depositAmount)).wait();
   }
 
-  const fee = await uniswap.fee();
   let currentPrice = wethPrice;
   let priceDelta = 0n;
 
@@ -63,7 +59,7 @@ export async function changeWethPrice(
     await swapRouter.connect(treasury).swapExactInput(uniswapV3Swapdata(), tokenIn, tokenOut, amountIn, 0);
 
     const { tick } = await uniswap.connect(treasury.provider).slot0();
-    const price = BigInt(tickToPrice(WETH, USDC, Number(tick)).toFixed(0));
+    const price = BigInt(await estimateTickToPrice(weth, usdc, Number(tick)));
     priceDelta = abs(price - currentPrice);
     currentPrice = price;
     logger.info(`  WETH price is ${currentPrice}`);
@@ -73,10 +69,26 @@ export async function changeWethPrice(
 
   {
     const { tick } = await uniswap.connect(treasury.provider).slot0();
-    const wethPrice = BigInt(tickToPrice(WETH, USDC, Number(tick)).toFixed(0));
+    const wethPrice = BigInt(await estimateTickToPrice(weth, usdc, Number(tick)));
     logger.info(`WETH price is ${wethPrice}`);
     logger.info(`uniswap WETH balance  is ${formatUnits(await weth.balanceOf(uniswap), 18)}`);
     logger.info(`uniswap USDC balance is ${formatUnits(await usdc.balanceOf(uniswap), 6)}`);
   }
   logger.info(`Price changed`);
+}
+
+async function estimateTickToPrice(
+  baseToken: IERC20Metadata,
+  quoteToken: IERC20Metadata,
+  tick: number
+): Promise<number> {
+  const price = Math.pow(1.0001, tick);
+
+  const baseDecimals = await baseToken.decimals();
+  const quoteDecimals = await quoteToken.decimals();
+  const factor = Math.pow(10, Number(baseDecimals - quoteDecimals));
+
+  return Math.floor(
+    baseToken.toString().toLowerCase() < quoteToken.toString().toLowerCase() ? price / factor : factor / price
+  );
 }
